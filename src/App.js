36dd25c6f0df703026c753eced5381ef8f3c546c
lengocson510534 <lengocson26112000@gmail.com>
2022-08-08 12:33:26 +0700

    upload source code

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import Icon from "./UI/Icon";
 
 function App() {
   const icon = [
-    { icon: "bx bxl-youtube" },
-    { icon: "bx bxl-facebook-circle" },
-    { icon: "bx bxl-instagram" },
+    { icon: "bx bxl-youtube", title: "youtube" },
+    { icon: "bx bxl-facebook-circle", title: "facebook" },
+    { icon: "bx bxl-instagram", title: "instagram" },
+    { icon: "bx bxl-react", title: "react" },
+    { icon: "bx bxl-javascript", title: "javascript" },
+    { icon: "bx bxl-tailwind-css", title: "tailwindcss" },
+    { icon: "bx bxl-css3", title: "css" },
+    { icon: "bx bxl-html5", title: "html" },
+    { icon: "bx bxl-redux", title: "redux" },
+    { icon: "bx bxl-c-plus-plus", title: "c++" },
   ];
   return (
     <div>
       <Header />
       <div className="list__icon">
         {icon.map((item, index) => (
-          <Icon icon={item.icon} key="index" />
+          <Icon icon={item.icon} key="index" title={item.title} />
         ))}
       </div>
     </div>
--- src/UI/Icon.js
+++ src/UI/Icon.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import "./icon.css";
 const Icon = (props) => {
   return (
     <div className="icon">
       <i className={props.icon}></i>
+      <span className="title">{props.title}</span>
     </div>
   );
 };
